fce454373b5a063da380e020226d8688a9943ab6
Zhanho Shing <zhanhoshing@icloud.com>
2021-01-12 07:38:38 +0800

    Added tool for creating file list

---
+++ ReleaseTool/frmMain.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace ReleaseTool {
+    partial class frmMain {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.treeFiles = new System.Windows.Forms.TreeView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusFilename = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeFiles
+            // 
+            this.treeFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeFiles.CheckBoxes = true;
+            this.treeFiles.Location = new System.Drawing.Point(12, 12);
+            this.treeFiles.Name = "treeFiles";
+            this.treeFiles.Size = new System.Drawing.Size(853, 585);
+            this.treeFiles.TabIndex = 0;
+            this.treeFiles.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeFiles_AfterSelect);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusFilename});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 629);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1159, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusFilename
+            // 
+            this.statusFilename.Name = "statusFilename";
+            this.statusFilename.Size = new System.Drawing.Size(57, 17);
+            this.statusFilename.Text = "FileName";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(12, 603);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(871, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(276, 585);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1159, 651);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.treeFiles);
+            this.Name = "frmMain";
+            this.Text = "發行版本工具";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeFiles;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusFilename;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ ReleaseTool/frmMain.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+
+namespace ReleaseTool {
+    public partial class frmMain : Form {
+        public frmMain() {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private string _dirPath = string.Empty;
+
+        private void frmMain_Load(object sender, EventArgs e) {
+            // Selects folder
+            var odd = new FolderBrowserDialog() {
+                Description = "Choose a directory"
+            };
+            if (odd.ShowDialog() == DialogResult.OK) {
+                _dirPath = odd.SelectedPath;
+            } else
+                Application.Exit();
+
+            var folder = new Folder(_dirPath);
+            treeFiles.Nodes.Add(folder.Node);
+        }
+
+        private void treeFiles_AfterSelect(object sender, TreeViewEventArgs e) {
+            var myNode = (MyNode) e.Node;
+            statusFilename.Text = myNode.Path;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e) {
+            var bgw = new BackgroundWorker() {
+                WorkerReportsProgress = false, WorkerSupportsCancellation = false
+            };
+            bgw.DoWork += (bgwSender, bgwE) => {
+                btnGenerate.Enabled = false;
+                richTextBox1.Enabled = false;
+                treeFiles.Enabled = false;
+
+                var myNode = (MyNode) treeFiles.Nodes[0];
+                var rootFolder = new JsonRootFolder(myNode, myNode.Path);
+                var json = JsonConvert.SerializeObject(rootFolder);
+                var results = new List<object>();
+                results.Add(json);
+                bgwE.Result = results;
+            };
+            bgw.RunWorkerCompleted += (bgwCSender, bgwCE) => {
+                btnGenerate.Enabled = true;
+                richTextBox1.Enabled = true;
+                treeFiles.Enabled = true;
+
+                var results = (List<object>) bgwCE.Result;
+                richTextBox1.Text = results[0].ToString();
+            };
+            bgw.RunWorkerAsync();
+        }
+    }
+
+    internal class Folder {
+        internal Folder(string path) {
+            Node = new MyNode(Path.GetFileName(path), path, MyNode.NodeType.Folder);
+
+            // Add sub nodes
+            foreach (var dir in Directory.GetDirectories(path)) Node.Nodes.Add(new Folder(dir).Node);
+
+            foreach (var file in Directory.GetFiles(path)) Node.Nodes.Add(new MyNode(Path.GetFileName(file), file, MyNode.NodeType.File));
+        }
+
+        internal MyNode Node;
+    }
+
+    internal class MyNode : TreeNode {
+        internal MyNode(string name, string path, NodeType type) : base(name) {
+            Path = path;
+            Checked = !new[] {".pdb", ".config"}.Contains(System.IO.Path.GetFileName(path)
+                .Replace(System.IO.Path.GetFileNameWithoutExtension(path), string.Empty));
+            TypeOfNode = type;
+        }
+
+        internal string Path { get; }
+        internal NodeType TypeOfNode { get; }
+
+        internal enum NodeType {
+            Folder, File
+        }
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    internal class JsonRootFolder {
+        internal JsonRootFolder(MyNode node, string root) {
+            foreach (MyNode myNode in node.Nodes) {
+                if (!myNode.Checked) continue;
+                if (myNode.TypeOfNode == MyNode.NodeType.Folder)
+                    JsonFolders.Add(new JsonFolder(myNode, root));
+                else 
+                    JsonFiles.Add(new JsonFile(myNode, root));
+            }
+        }
+        
+        [JsonProperty("folders")]
+        internal List<JsonFolder> JsonFolders = new List<JsonFolder>();
+        [JsonProperty("files")]
+        internal List<JsonFile> JsonFiles = new List<JsonFile>();
+
+        [JsonObject(MemberSerialization.OptIn)]
+        internal class JsonFolder : JsonRootFolder {
+            internal JsonFolder(MyNode node, string root) : base(node, root) {
+                RelativePath = node.Path.Replace(root, string.Empty);
+            }
+
+            [JsonProperty("path")]
+            internal string RelativePath;
+        }
+
+        [JsonObject(MemberSerialization.OptIn)]
+        internal class JsonFile {
+            internal JsonFile(MyNode node, string root) {
+                RelativePath = node.Path.Replace(root, string.Empty);
+                using (var md5 = MD5.Create())
+                using (var stream = File.OpenRead(node.Path)) {
+                    var hash = md5.ComputeHash(stream);
+                    MD5Value = BitConverter.ToString(hash).Replace("-", "").ToUpper();
+                }
+            }
+
+            [JsonProperty("path")]
+            internal string RelativePath;
+            [JsonProperty("md5")]
+            internal string MD5Value;
+        }
+    }
+}
